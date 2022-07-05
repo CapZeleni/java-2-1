f708226325215d2a6f98600891fb21b4eaec28c9
Николай <Filippovnikolya@yandex.ru>
2022-07-05 21:13:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int mileruble = 20; // 1 миля за 20 рублей
+        int ruble = 1000; // цена билета в рублях
+
+        System.out.println("Купили билет получили бонус в милях:");
+        System.out.println((ruble / mileruble) + " бонусных миль");
+
+    }
+}
